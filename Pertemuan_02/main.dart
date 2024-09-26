090774d9acf497cbe04767063197b9a6da1ed3d4
Alfian Mutakim <alfianmutaqien@gmail.com>
2024-09-27 00:04:11 +0700

    update tugas

--- Pertemuan_02/main.dart
+++ Pertemuan_02/main.dart
@@ -19,7 +19,44 @@
 
 // }
 
+// void main() {
+//   List<dynamic> namaMahasiswa = ["alfian", 2, false, ["makan", "olahraga"]];
+//   print(namaMahasiswa[5]);
+// }
+
 void main() {
-  List<dynamic> namaMahasiswa = ["alfian", 2, false, ["makan", "olahraga"]];
-  print(namaMahasiswa[5]);
+  List<Map<String, dynamic>> daftarMahasiswa = [
+    {
+      "nama": "John Doe",
+      "ipk": 3.88,
+      "isMarried": false,
+    },
+    {
+      "nama": "Andi Doe",
+      "ipk": 3.78,
+      "isMarried": true,
+    },
+    {
+      "nama": "Budi Doe",
+      "ipk": 3.68,
+      "isMarried": true,
+    }
+  ];
+
+  // 1.a. Menghitung rerata dari IPK
+  double semuaIpk = 0;
+  for (var mahasiswa in daftarMahasiswa) {
+    semuaIpk += mahasiswa['ipk'];
+  }
+  double ratarataIpk = semuaIpk / daftarMahasiswa.length;
+  print('Rata-rata IPK: $ratarataIpk');
+
+  // 1.b. Menjumlahkan berapa banyak mahasiswa yang sudah menikah
+  int dataNikah = 0;
+  for (var mahasiswa in daftarMahasiswa) {
+    if (mahasiswa['isMarried']) {
+      dataNikah++;
+    }
+  }
+  print('Jumlah mahasiswa yang sudah menikah: $dataNikah');
 }
