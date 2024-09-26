c6b2d6cee864230eae2b979302b53e073c5c967b
Alfian Mutakim <alfianmutaqien@gmail.com>
2024-09-26 23:09:42 +0700

    update

---
+++ Pertemuan_02/main.dart
@@ -0,0 +1,25 @@
+// void main () {
+//   String namaMahasiswa; //Deklarasi
+//   double ipk = 3.99; //inisialisasi
+
+  // bool isStudent = true;
+
+  // int umur = 20;
+
+  // String namaHewan = "ikan";
+  // print(ipk);
+
+  // var nilai = 90;
+  // nilai="satu";
+  // print(nilai);
+
+  // dynamic alamat = "Wsb";
+  // alamat="80";
+  // print(alamat);
+
+// }
+
+void main() {
+  List<dynamic> namaMahasiswa = ["alfian", 2, false, ["makan", "olahraga"]];
+  print(namaMahasiswa[5]);
+}
